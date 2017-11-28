388ebdcb61b873ae20651744110d448953871de2
Jose Antonio Gil <joseantonio.gil@intelygenz.com>
2017-11-28 14:33:46 +0100

    Use HTTPS weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 
 const OWM_API_KEY = 'cce420b78c35e958844f8e4cd0048d56';
-const API_URL = `http://api.openweathermap.org/data/2.5/find?units=metric&appid=${OWM_API_KEY}&q=`;
+const API_URL = `https://api.openweathermap.org/data/2.5/find?units=metric&appid=${OWM_API_KEY}&q=`;
 
 @Injectable()
 export class WeatherService {
